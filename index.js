d421d3312115d7a5f255a59498d0986cc963266c
Keith Burgie <krburgie@gmail.com>
2019-04-18 04:25:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,8 +57,8 @@ function removeFromCart(item) {
   return `That item is not in your cart.`;
 }
 
+var total;
 function placeOrder(cardNumber) {
-  var total;
   if (!cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`;
   } else {
