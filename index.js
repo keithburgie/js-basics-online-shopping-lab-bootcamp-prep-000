c18203a55a0ed9d050131767c99e49f521a3300c
Keith Burgie <krburgie@gmail.com>
2019-04-18 04:17:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,9 +59,11 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if (!cardNumber) {
-    return `Sorry, we don't have a credit card on file for you.`
+    return `Sorry, we don't have a credit card on file for you.`;
   } else {
     cart = [];
-    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`
+    console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+    return `Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`;
   }
 }
+placeOrder(2342343);
