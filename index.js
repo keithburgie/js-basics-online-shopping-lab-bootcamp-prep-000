12fd13d142d0a38817d473e81c068352f3dfd218
Keith Burgie <krburgie@gmail.com>
2019-04-18 04:33:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -59,7 +59,6 @@ function placeOrder(cardNumber) {
   if (!cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`;
   } else {
-    total();
     cart = [];
     return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
   }
